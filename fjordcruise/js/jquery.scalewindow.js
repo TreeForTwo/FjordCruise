6d91ff6f3198d7603f550320d4c7fbea65d509d3
RandomGamer342 <rob.342@hotmail.com>
2014-05-12 11:53:35 +0200

    Revert "Gradient Clamp 2: Electric Boogaloo"
    This reverts commit e68c2cc9f2f9d2f12a284816a868beb24b417cda.

--- fjordcruise/js/jquery.scalewindow.js
+++ fjordcruise/js/jquery.scalewindow.js
@@ -24,6 +24,7 @@ function ScaleContent() {
 	var background = $("#backgroundgradient");
 	var backgroundheight = Math.min( $(window).height(), 600 );
 	background.height(backgroundheight);
+	$("#backgroundwrap").height(backgroundheight);
 }
 
 function ScaleIndex() {
